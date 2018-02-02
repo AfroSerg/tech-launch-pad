f6f1663ee001441b6dfa6e89b6d3b7c0508d9f40
Lesego <lsgmotea@live.com>
2018-02-02 17:27:30 +0200

    bruh

--- js/site.js
+++ js/site.js
@@ -1,11 +1,13 @@
 $(".item-info").click(() => {
     console.log('onlick fired')
     $('.global-content').addClass('info-active');
+    $('.global-footer').addClass('info-active');
     $('#info-section').delay(300).addClass('animate');
 })
 
 $(".item-home").click(() => {
     console.log('onclick fired')
     $('.global-content').removeClass('info-active');
+    $('.global-footer').removeClass('info-active');
     $('#info-section').removeClass('animate');
 })
