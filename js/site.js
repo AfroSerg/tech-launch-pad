76e2417ae8bd54164d6172162a22f00feb44a976
Lesego <lsgmotea@live.com>
2018-02-04 15:36:30 +0200

    hey more stufs

--- js/site.js
+++ js/site.js
@@ -2,6 +2,8 @@ $(".item-info").click(() => {
     console.log('onlick fired')
     $('.global-content').addClass('info-active');
     $('.global-footer').addClass('info-active');
+    $('.nav-section.secondary>.item-info').css('display', 'none')
+    $('.nav-section.secondary>.item-home').css('display', 'inline-block')
     $('#info-section').delay(300).addClass('animate');
 })
 
@@ -9,5 +11,7 @@ $(".item-home").click(() => {
     console.log('onclick fired')
     $('.global-content').removeClass('info-active');
     $('.global-footer').removeClass('info-active');
+    $('.nav-section.secondary>.item-home').css('display', 'none')
+    $('.nav-section.secondary>.item-info').css('display', 'inline-block')
     $('#info-section').removeClass('animate');
 })
